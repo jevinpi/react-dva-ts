7e33bdbc0e9909749de1ebddc0429e5e47309149
皮健云 <pijianyun@ascs.tech>
2020-05-29 17:52:58 +0800

    feat: 更新stylelint相关

--- scripts/prettier.js
+++ scripts/prettier.js
@@ -14,7 +14,7 @@ const prettierConfigPath = require.resolve('../.prettierrc');
 let didError = false;
 
 let files = [];
-const scssFiles = glob.sync('src/**/*.scss', { ignore: ['**/node_modules/**', 'build/**'] });
+const scssFiles = glob.sync('src/**/*.less', { ignore: ['**/node_modules/**', 'build/**'] });
 const tsFiles = glob.sync('src/**/*.ts*', { ignore: ['**/node_modules/**', 'build/**'] });
 files = files.concat(scssFiles);
 files = files.concat(tsFiles);
